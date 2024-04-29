a579e8fd6464aede461bc48bff376345fb2c06d9
athakur7 <anandthakur178@gmail.com>
2024-04-29 13:11:10 +0000

    lightning studio workflow

---
+++ install_kind.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+echo "APT Updated."
+# Check if Docker is installed
+if ! [ -x "$(command -v docker)" ]; then
+  echo "Docker is not installed. Please install Docker first."
+  exit 1
+fi
+
+# Install Kind
+echo "Installing Kind..."
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+echo "Kind installation complete."
+
+# Install Kubectl
+sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+# If the folder `/etc/apt/keyrings` does not exist, it should be created before the curl command, read the note below.
+# sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
+
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
+
+sudo apt-get update
+sudo apt-get install -y kubectl
---
+++ lightning_studio_active.py
@@ -0,0 +1,21 @@
+#! pip install lightning-sdk
+import os
+from lightning_sdk import Studio
+
+# run from inside a Studio: control the studio you are running on
+# s = Studio()
+# s.stop()
+
+# Retrieve values from GitHub Secrets
+studio_name = os.environ['LIGHTNING_STUDIO_NAME']
+teamspace = os.environ['LIGHTNING_TEAMSPACE']
+user = os.environ['LIGHTNING_USER']
+
+# Create a new Studio instance with secrets
+s = Studio(name=name, teamspace=teamspace, user=user, create_ok=True)
+# before we call start, the Studio exists but isn't running
+s.start()
+output1 = s.run("ls")
+print(output1)
+output2 = s.run("python3 main.py")
+print(output2)
